28b14f0ab79717730d9e0d90194c36ce95c2fed9
LOZORD <ljrudberg@gmail.com>
2014-04-01 18:35:15 -0500

    Generated new Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Numberluncher::Application.config.secret_key_base = '190a52c18b83391aa06caf10a10463bf9999212bc6238e94d64253288c8d2e0d5cb6a4576606576a693c35705caf84b85309baddaed19f109cf9356bcfdba4a3'
