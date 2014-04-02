e6ee9f42f965cf577a0e1399a4a7b51af19e26dd
LOZORD <ljrudberg@gmail.com>
2014-04-01 19:03:40 -0500

    Added schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140401235725) do
+
+  create_table "observations", force: true do |t|
+    t.datetime "date_time"
+    t.integer  "line_size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
