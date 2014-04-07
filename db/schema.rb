7e45eaad08cd1b367de42098b087e57a9e40ee9c
LOZORD <ljrudberg@gmail.com>
2014-04-06 20:23:52 -0500

    Added password digest to User, working on authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407010050) do
+ActiveRecord::Schema.define(version: 20140407011835) do
 
   create_table "observations", force: true do |t|
     t.datetime "date_time"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20140407010050) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
