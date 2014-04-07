f8eeea144c220e7aabcecbdce27b43d96e0466e9
chelseydenton <dentonchelsey@gmail.com>
2014-04-06 19:24:25 -0500

    added bootstrap and header and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def signed_in?
+    false
+  end
+
+	# Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Numberluncher"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
