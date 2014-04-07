01f0318ad6249f5412cb030235dd754c7f332cc0
LOZORD <ljrudberg@gmail.com>
2014-04-06 21:08:02 -0500

    More changes to allow signup/in

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Numberluncher::Application.routes.draw do
 
   root 'static_pages#home'
 
+  match '/signup',  to: 'users#new',  via:  'get'
+  match '/signin',  to: 'sessions#new',   via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
