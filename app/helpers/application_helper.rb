4a302540958cff35e9fd3e5f67305468e4ba0a3b
chelseydenton <dentonchelsey@gmail.com>
2014-04-06 21:30:28 -0500

    sign in sign out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def signed_in?
-    false
-  end
-
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
     base_title = "Numberluncher"
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Numberluncher::Application.routes.draw do
 
   resources :observations
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   root 'static_pages#home'
 
   match '/signup',  to: 'users#new',  via:  'get'
   match '/signin',  to: 'sessions#new',   via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
