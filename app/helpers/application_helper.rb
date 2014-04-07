71ad73df5c693b71954b88cb8151528b84b2c481
LOZORD <ljrudberg@gmail.com>
2014-04-06 21:30:05 -0500

    Verified new user sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Numberluncher"
+    base_title = "NumberLuncher"
     if page_title.empty?
       base_title
     else
