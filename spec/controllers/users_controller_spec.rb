290931a9ed55524e3012a0a35386402ef3eed117
chelseydenton <dentonchelsey@gmail.com>
2014-04-07 21:58:25 -0500

    fix user controller spec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -23,7 +23,8 @@ describe UsersController do
   # This should return the minimal set of attributes required to create a valid
   # User. As you add validations to User, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "email" => "MyString" } }
+  let(:valid_attributes) { { "email" => "adf@adf.com", "password" => "password",
+      "password_confirmation" => "password" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
