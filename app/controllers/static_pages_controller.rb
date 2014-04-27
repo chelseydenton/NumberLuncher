f7e9448fb5be5e67aa52595aec64bb4a9d809925
chelseydenton <dentonchelsey@gmail.com>
2014-04-27 15:55:49 -0500

    show restaurant info on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
-
+	include SessionsHelper
   def home
+  	if signed_in?
+      @restaurants = Restaurant.paginate(page: params[:page])
+    end
   end
 
   def help
