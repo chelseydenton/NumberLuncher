c8c0ed908fe8a645633338782f4d26ecd32cc4a1
LOZORD <ljrudberg@gmail.com>
2014-04-28 22:11:17 -0500

    Forgot to add location req to controller spec

--- spec/controllers/restaurants_controller_spec.rb
+++ spec/controllers/restaurants_controller_spec.rb
@@ -23,7 +23,7 @@ describe RestaurantsController do
   # This should return the minimal set of attributes required to create a valid
   # Restaurant. As you add validations to Restaurant, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "name" => "MyString" } }
+  let(:valid_attributes) { { "name" => "MyString", "location" => "MyLoc" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
