e059179552e1f2fb45a1559bf3e004d5226e482e
chelseydenton <dentonchelsey@gmail.com>
2014-05-07 20:43:55 -0500

    high charts partially working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,14 @@
 class Restaurant < ActiveRecord::Base
   validates :name, presence: true
   validates :location, presence: true
-	has_many :observations
+  has_many :observations
+
+	def self.average_on(date, id)
+		@temp_obs = Observation.where("date(date_time) = ? AND restaurant_id = ?", date, id)
+		if @temp_obs.nil?
+			0
+		else
+			@temp_obs.average(:line_size)
+		end
+	end
 end
