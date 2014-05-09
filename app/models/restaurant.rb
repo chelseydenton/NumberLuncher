c126762f0c1b0deae5cd0c18e81aa76f98cf422a
chelseydenton <dentonchelsey@gmail.com>
2014-05-08 19:06:39 -0500

    highcharts working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,12 +3,16 @@ class Restaurant < ActiveRecord::Base
   validates :location, presence: true
   has_many :observations
 
-	def self.average_on(date, id)
-		@temp_obs = Observation.where("date(date_time) = ? AND restaurant_id = ?", date, id)
-		if @temp_obs.nil?
-			0
-		else
-			@temp_obs.average(:line_size)
-		end
+	def self.PastDaysObservations(id)
+
+	    observations_array = []
+	    Observation.where("date_time > ? AND restaurant_id = ?", DateTime.now - 1, id).order("date_time").reverse_order.each do |obs|
+	    	tmp_array = []
+	    	tmp_array.push(obs.date_time.to_i*1000)
+	    	tmp_array.push(obs.line_size)
+	        observations_array.push(tmp_array)
+	    end
+
+	    return observations_array
 	end
 end
